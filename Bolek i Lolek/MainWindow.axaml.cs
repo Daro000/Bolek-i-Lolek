ade82b8b4ced6fba3c1831a01e6a6342fbc0c4b9
Daro <twojemail@example.com>
2025-09-10 19:05:44 +0200

    kontynuacja

--- Bolek i Lolek/MainWindow.axaml.cs
+++ Bolek i Lolek/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
 
 namespace Bolek_i_Lolek;
 
@@ -16,13 +17,40 @@ public partial class MainWindow : Window
         "avares://Bolek i Lolek/Assets/Lolek.webp"
     };
     
-    string comboBoxValue = (MyComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
     
-    private void ShowBolekLolek(object sender, RoutedEventArgs e)
+    private void MyComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
-        try
+        if (MyComboBox.SelectedIndex >= 0)
         {
-            if
+            var path = _imagePaths[MyComboBox.SelectedIndex];
+            ShowImage.Source = new Bitmap(path);
+        }
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        string nazwaZadania = Zadanie.Text?.Trim() ?? "";
+        if(string.IsNullOrWhiteSpace(nazwaZadania))
+            return;
+
+        string priotytety = "normalny";
+        if (NiskiPriorytet.IsChecked == true) priotytety = "niski";
+        else if(WysokiPriorytet.IsChecked == true) priotytety = "wysoki";
+        
+        string bohater = (MyComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ??  "Nie znany lub nie wybrany bohater";
+
+        string zadanieBoh = $"{bohater} - {nazwaZadania} - {priotytety}";
+        
+        ListaZadan.Items.Add(zadanieBoh);
+        
+        Zadanie.Text = "";
+    }
+
+    private void Usun_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (ListaZadan.SelectedItem != null)
+        {
+            ListaZadan.Items.Remove(ListaZadan.SelectedItem);
         }
     }
 }
